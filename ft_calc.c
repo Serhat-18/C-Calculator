89481a915a391aa52dbe754b44a2d6cdd1af754f
Serhat-18 <79026362+Serhat-18@users.noreply.github.com>
2025-03-17 03:49:27 +0300

    Add files via upload

--- ft_calc.c
+++ ft_calc.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_calc.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: sekaska <sekaska@student.42kocaeli.com.tr  +#+  +:+       +#+        */
+/*   By: sekaska <sekaska@student.42kocaeli.com.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/16 20:11:07 by sekaska           #+#    #+#             */
-/*   Updated: 2025/03/16 20:34:40 by sekaska          ###   ########.tr       */
+/*   Updated: 2025/03/17 03:49:23 by sekaska          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -87,66 +87,63 @@ long	ft_subtraction(int num1, int num2)
 }
 
 /*
- 
-
 int main (void) {
 	int	num1, num2;
 	char	operator;
 
-    printf("Bir işlem girin (örn: !, +, -, *, /, %%): ");
+    printf("Bir islem girin (orn: !, +, -, *, /, %%): ");
     scanf(" %c", &operator);
 
     if (operator == '!') {
-        printf("Bir sayı girin: ");
+        printf("Bir sayi girin: ");
         if (scanf("%d", &num1) == 1) {
-            printf("%d sayısının faktöriyel sonucu: %ld\n", num1, ft_factorial(num1));
+            printf("%d sayisinin faktoriyel sonucu: %ld\n", num1, ft_factorial(num1));
         } else {
-            printf("Geçersiz giriş!\n");
+            printf("Gecersiz giris!\n");
         }
     } else if (operator == '+') {
-        printf("İki sayı girin: ");
+        printf("İki sayi girin: ");
         if (scanf("%d %d", &num1, &num2) == 2) {
-            printf("Sonuç: %ld\n", ft_addition(num1, num2));
+            printf("Sonuc: %ld\n", ft_addition(num1, num2));
         } else {
-            printf("Geçersiz giriş!\n");
+            printf("Gecersiz giris!\n");
         }
     } else if (operator == '-') {
-        printf("İki sayı girin: ");
+        printf("İki sayi girin: ");
         if (scanf("%d %d", &num1, &num2) == 2) {
-            printf("Sonuç: %ld\n", ft_subtraction(num1, num2));
+            printf("Sonuc: %ld\n", ft_subtraction(num1, num2));
         } else {
-            printf("Geçersiz giriş!\n");
+            printf("Gecersiz giris!\n");
         }
     } else if (operator == '*') {
-        printf("İki sayı girin: ");
+        printf("İki sayi girin: ");
         if (scanf("%d %d", &num1, &num2) == 2) {
-            printf("Sonuç: %ld\n", ft_multiplication(num1, num2));
+            printf("Sonuc: %ld\n", ft_multiplication(num1, num2));
         } else {
-            printf("Geçersiz giriş!\n");
+            printf("Gecersiz giris!\n");
         }
     } else if (operator == '/') {
-        printf("İki sayı girin: ");
+        printf("İki sayi girin: ");
         if (scanf("%d %d", &num1, &num2) == 2) {
             if (num2 != 0) {
-                printf("Sonuç: %.6f\n", (double)num1 / num2);
+                printf("Sonuc: %.6f\n", (double)num1 / num2);
             } else {
-                printf("Hata: Bölme sıfıra yapılamaz!\n");
+                printf("Hata: Bolme sifira yapilamaz!\n");
             }
         } else {
-            printf("Geçersiz giriş!\n");
+            printf("Gecersiz giris!\n");
         }
     } else if (operator == '%') {
-        printf("İki sayı girin: ");
+        printf("İki sayi girin: ");
         if (scanf("%d %d", &num1, &num2) == 2) {
-            printf("Sonuç: %ld\n", ft_mod(num1, num2));
+            printf("Sonuc: %ld\n", ft_mod(num1, num2));
         } else {
-            printf("Geçersiz giriş!\n");
+            printf("Gecersiz giris!\n");
         }
     } else {
-        printf("Geçersiz işlem! Lütfen geçerli bir işlem girin.\n");
+        printf("Gecersiz islem! Lütfen gecerli bir islem girin.\n");
     }
 
     return 0;
 }
-
 */
